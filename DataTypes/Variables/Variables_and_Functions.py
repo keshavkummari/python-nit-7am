bfe88b4b90fcd34405742c590224abf9a1375d21
keshavkummari <keshav.kummari@gmail.com>
2018-10-03 07:47:54 +0530

    Variables

---
+++ DataTypes/Variables/Variables_and_Functions.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3.6
+
+# Declaring or Creating Variables
+var = 10            # Integer Variable
+var1 = 10.25        # Float
+var2 = 'Python'     # String
+var3 = "Django"
+var4 = """Anacodna
+Spyder"""
+var5 = '''Pycharm
+Spyder'''
+
+"""
+Multi-Line
+Comments
+using
+Double Triples
+"""
+
+'''
+Multi-Line
+Comments
+using single Triples
+'''
+# Accessing variables
+print(var,type(var),id(var))
+print(var1,type(var1),id(var1))
+print(var2,type(var2),id(var2))
+print(var3,type(var3),id(var3))
+print(var4,type(var3),id(var4))
+print(var5,type(var5),id(var5))
+
+# Creatig variables and accessing them using print()
+Abc,cde,doll,tools,idel=10,20.10,3+4j,"pycharm","spider"
+
+print(Abc,type(Abc),id(Abc))
+print(cde,type(cde),id(cde))
+print(doll,type(doll),id(doll))
+print(tools,type(tools),id(tools))
+print(idel,type(idel),id(idel))
+
+# Creatig variables and accessing them using print()
+var10=var20=var30=100
+
+print(var10,type(var10),id(var10))
+print(var20,type(var20),id(var20))
+print(var30,type(var30),id(var30))
